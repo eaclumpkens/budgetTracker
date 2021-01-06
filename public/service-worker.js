188cacd40207d44c532dac893f625b9c5b8c9998
eaclumpkens <eaclumpkens@gmail.com>
2021-01-06 13:28:16 -0500

    updated cache files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',
-    '/index.js',
-    '/style.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    './index.html',
+    './index.js',
+    './style.css',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    './manifest.webmanifest'
 ];
 
 const CACHE_NAME = 'budget-cache-v1';
